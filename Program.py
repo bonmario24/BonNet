49a37ead83af90a4ca31bb3e62f20bc42c417b5d
Bonmario <bonmariocontact@gmail.com>
2021-11-18 16:46:51 -0500

    Initial commit

---
+++ Program.py
@@ -0,0 +1,30 @@
+##  this is a ip masker for ipv4 and ipv6 for arm macm1
+print("Would you like to change your ip address and make it so no one can trace you?")
+import socket
+import ipaddress
+import re
+import sys
+import os
+## make a random ip address from the world and generate it again
+sys.ip = "000.00.00.00"
+sys.ip_version = "Bonpeach Relay Network"
+sys.ip_network = "Bon Network 1.1.1.1.1"
+## patch the ipv4 address to the sys.ip
+ipv4 = "127.0.9.55.6"
+IPV6 = "55.55a:d"
+sys.ip_network = "Bon tech LLC"
+country = "Russia"
+DNS = "8.8.8.8"
+
+## change the wifi router ip address
+ipaddress.set = "777.7.7.77"
+
+### make a router masker
+router = ipaddress.ip_network
+## make a random ip address from the world and generate it again
+ipv4 = "124.5245.9.55"
+STATE = "New York"
+Zipcode = "5245"
+ORGANIZATION = "Bon Tech INC"
+import sys
+ 
---
+++ README.md
@@ -0,0 +1,4 @@
+# BonNet
+The protocol for bonmario inc
+
+Download Python And Run The Python File.
